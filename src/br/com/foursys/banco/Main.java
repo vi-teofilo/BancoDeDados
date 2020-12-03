ebf5065db08bf43700ae7eff6b1dc49d0c29d744
Vitória Teófilo Furtado <vfurtado@server01.foursys.local>
2020-12-03 15:41:57 -0300

    Banco de Dados

---
+++ src/br/com/foursys/banco/Main.java
@@ -0,0 +1,25 @@
+
+package br.com.foursys.banco;
+
+import br.com.foursys.banco.view.AlunoAlterarView;
+import br.com.foursys.banco.view.AlunoExcluirView;
+import br.com.foursys.banco.view.AlunoInserirView;
+import br.com.foursys.banco.view.AlunoListarView;
+import br.com.foursys.banco.view.MenuView;
+
+/**
+ *
+ * @author vfurtado
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+//        new AlunoInserirView();
+//    new AlunoAlterarView();    
+//        new AlunoExcluirView();
+       // new AlunoListarView();
+        new MenuView();
+    }
+    
+}
